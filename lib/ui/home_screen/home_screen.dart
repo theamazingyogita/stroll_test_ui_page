5675ca2e32fa04b62af26897731147e124c11665
Yogita Agarwal <theweirdyoyo@gmail.com>
2025-01-08 18:05:35 +0530

    Added the assets and created the base structure of the project. Added the necessary packages.

---
+++ lib/ui/home_screen/home_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 4,
+      child: SafeArea(
+        bottom: true,
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            controller: _tabController,
+            children: [
+              Stack(
+                children: [
+                  Image.asset(
+                    'assets/background video.png',
+                    alignment: Alignment.topCenter,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    top: kToolbarHeight,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Stroll Bonfire',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.novaSquare().copyWith(
+                          color: Color(0xffB3ADF6),
+                          fontSize: 34,
+                          fontWeight: FontWeight.bold,
+                          shadows: <Shadow>[
+                            Shadow(
+                              blurRadius: 3.0,
+                              color: Color(0x000000ff),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(),
+              Container(),
+              Container(),
+            ],
+          ),
+          bottomNavigationBar: TabBar(
+            padding: EdgeInsets.only(
+              bottom: 20,
+            ),
+            automaticIndicatorColorAdjustment: true,
+            dividerColor: Colors.transparent,
+            indicatorColor: Colors.transparent,
+            controller: _tabController,
+            labelColor: Colors.black45,
+            tabs: [
+              SvgPicture.asset('assets/swap.svg'),
+              SvgPicture.asset('assets/bonfire.svg'),
+              Image.asset('assets/chat.png'),
+              SvgPicture.asset('assets/User.svg')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
